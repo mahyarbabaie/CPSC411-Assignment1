2761ffa72728cea3976cb0ad9d8b9efb024d7a7d
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-09-17 15:26:41 -0700

    Add files via upload

---
+++ Student.h
@@ -0,0 +1,24 @@
+//
+//  Student.h
+//  Assignment1
+//
+//  Created by Mahyar Babaie on 9/17/16.
+//  Copyright © 2016 Mahyar Babaie. All rights reserved.
+//
+
+#ifndef Student_h
+#define Student_h
+
+@interface Student : Person
+{
+    NSArray *stock;
+    
+}
+
+
+
+
+
+
+@end
+#endif /* Student_h */
---
+++ Student.m
@@ -0,0 +1,9 @@
+//
+//  Student.m
+//  Assignment1
+//
+//  Created by Mahyar Babaie on 9/17/16.
+//  Copyright © 2016 Mahyar Babaie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
